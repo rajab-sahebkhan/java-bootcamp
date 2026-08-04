b36f0d32c4a1f83c5fed03b75f53f00f77716395
Rajab Sahebkhan <sahebkhanrajab@gmail.com>
2026-08-04 21:52:45 +0200

    Create Generics example
    - Added generic Box class
    - Implemented generic getter and setter methods
    - Demonstrated type-safe programming using Java generics

---
+++ Generics/Box.java
@@ -0,0 +1,19 @@
+package Generics;
+
+public class Box<T> {
+
+    private T item;
+
+    public Box(T item){
+        this.item = item;
+    }
+
+    public T getItem(){
+        return item;
+    }
+
+    public void setItem(T item){
+        this.item = item;
+    }
+
+}
